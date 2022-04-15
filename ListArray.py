409768cd64c0ca11fe170a8ac247a3dc37b05980
alif <alifrazanit@gmail.com>
2022-04-15 15:37:16 +0700

    listArray

---
+++ ListArray.py
@@ -0,0 +1,16 @@
+names = ["alif", "razan", "saputra"]
+print(names[0:1])
+print(names[-1])
+
+numbers = [1,2,3,4,5]
+
+numbers.append(100)
+numbers.insert(2, 88)
+
+numbers.pop(0)
+
+numbers.remove(3)
+
+numbers.sort()
+
+print(numbers)
