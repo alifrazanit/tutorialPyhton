c406ea1699307e0c04737045a319f706837a833a
alif <alifrazanit@gmail.com>
2022-04-15 15:33:08 +0700

    forLoop

---
+++ ForLoop.py
@@ -0,0 +1,12 @@
+arrayNilai = [1,2,3,4,5]
+
+# for item in arrayNilai:
+#     print(item) 
+
+
+for intem in range(10):
+    print(intem)
+
+
+for items in range(1, 12, 2): # 1 sampai 11 dengan step 2
+    print("xx", items)
