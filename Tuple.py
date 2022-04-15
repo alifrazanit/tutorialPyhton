291b6960f65d97045c8341ac7f90560d7bcce903
alif <alifrazanit@gmail.com>
2022-04-15 15:45:14 +0700

    tupple

---
+++ Tuple.py
@@ -0,0 +1,15 @@
+numbers = (1,2,3,4,5)
+# immutable
+
+#unpack Tuple
+
+# x = numbers[0]
+# z = numbers[1]
+# c = numbers[2]
+
+
+q, w, e, r, t = numbers # unpacknya harus sesuai jumlah tuple
+
+# print(x, z, c)
+
+print(q)
