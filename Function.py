576ac5b20f02613efd474f5a4b65d4903b4a34ad
alif <alifrazanit@gmail.com>
2022-04-15 16:08:14 +0700

    function

--- Function.py
+++ Function.py
@@ -1,3 +1,15 @@
-# len
-kalimat = 'lorem ipsum dolor sit amet'
-print(len(kalimat))
+def hallo(): 
+    print("haaiiiii" * 100)
+
+
+def Call(name):
+    print(f"Nama aku {name}")
+
+def menghitung(panjang, lebar, tinggi):
+    print(f"panjang : {panjang}")
+    print(f"lebar : {lebar}")
+    print(f"tinggi : {tinggi}")
+
+Call("Aini")
+
+menghitung(panjang=2, lebar=10,tinggi=4)
